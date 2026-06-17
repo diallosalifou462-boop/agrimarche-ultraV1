b25585afb39f0b2d0574fe03d918c3cfa6ad7a4e
Salifou Diallo <diallosalifou462@gmail.com>
2026-06-17 18:51:46 +0000

    Restore full project

--- next.config.js
+++ next.config.js
@@ -1,33 +1,38 @@
+const withPWA = require("next-pwa")({
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === "development",
+});
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  turbopack: {},
+
   images: {
     remotePatterns: [
-      // Firebase Storage
       {
-        protocol: 'https',
-        hostname: 'firebasestorage.googleapis.com',
-      },
-      // Unsplash (si tu utilises des images de test)
-      {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
-      },
-      // Cloudinary (optionnel)
-      {
-        protocol: 'https',
-        hostname: '*.cloudinary.com',
-      },
-      // Imgbb, imgur (optionnel)
-      {
-        protocol: 'https',
-        hostname: 'i.ibb.co',
+        protocol: "https",
+        hostname: "firebasestorage.googleapis.com",
       },
       {
-        protocol: 'https',
-        hostname: 'i.imgur.com',
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
+      {
+        protocol: "https",
+        hostname: "*.cloudinary.com",
+      },
+      {
+        protocol: "https",
+        hostname: "i.ibb.co",
+      },
+      {
+        protocol: "https",
+        hostname: "i.imgur.com",
       },
     ],
   },
 };
 
-module.exports = nextConfig;
+module.exports = withPWA(nextConfig);
